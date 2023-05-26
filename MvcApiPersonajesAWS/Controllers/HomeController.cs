d8179f41783f1ea64ebc6b98eec53b8e363dd7bb
fgarcia211 <fergarcia002100@outlook.es>
2023-05-26 10:42:13 +0200

    Add project files.

---
+++ MvcApiPersonajesAWS/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiPersonajesAWS.Models;
+using MvcApiPersonajesAWS.Services;
+using System.Diagnostics;
+
+namespace MvcApiPersonajesAWS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ServiceApiPersonajes service;
+
+        public HomeController(ServiceApiPersonajes service)
+        {
+            this.service = service;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ApiPersonajes()
+        {
+            List<Personaje> personajes =
+                await this.service.GetPersonajesAsync();
+            return View(personajes);
+        }
+    }
+}
